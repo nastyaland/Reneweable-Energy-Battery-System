55ec42ccc66ffa1bce6c9c649944204172887a62
nastyaland <anastasiadimov2002@gmail.com>
2026-03-13 17:26:12 -0400

    added libraries

---
+++ solarPanel.py
@@ -0,0 +1,8 @@
+import math
+import pandas as pd
+import plotly.graph_objects as go
+
+import pvlib
+from pvlib.modelchain import ModelChain
+from pvlib.pvsystem import PVSystem
+from pvlib.location import Location
