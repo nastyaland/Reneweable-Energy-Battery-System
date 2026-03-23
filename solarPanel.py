9169f1d31c3d8c64cc8a533eedfd6d9a81c04cab
nastyaland <anastasiadimov2002@gmail.com>
2026-03-22 20:23:21 -0400

    Added Data and Simulation Results

--- solarPanel.py
+++
@@ -1,8 +0,0 @@
-import math
-import pandas as pd
-import plotly.graph_objects as go
-
-import pvlib
-from pvlib.modelchain import ModelChain
-from pvlib.pvsystem import PVSystem
-from pvlib.location import Location
